99388a9e65cb2c67b23f40ff49843c2a57efd0ad
Rafael Benevides <rafabene@gmail.com>
2015-10-06 00:04:53 -0400

    Linux Fedora adjust

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #Run ansible
-ansible-playbook -i ansible/inventory ansible/setup-dotfiles.yaml --extra-vars "dotfilespath=`pwd`"
+ansible-playbook --ask-become-pass -i ansible/inventory ansible/setup-dotfiles.yaml --extra-vars "dotfilespath=`pwd`"
 
 #Run zsh
 env zsh
